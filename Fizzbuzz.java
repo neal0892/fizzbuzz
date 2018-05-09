4618508adad358c991bbf6ec036790ec3033543c
Neeraj Pandey <neal0892@gmail.com>
2018-05-09 13:44:48 +0530

    fixed indents

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -5,22 +5,22 @@ public class Fizzbuzz {
     public static void main(String[] args) throws InterruptedException {
             
             Scanner  s= new Scanner(System.in);
-                int input = s.nextInt();
+              int input = s.nextInt();
                     
-                    for(int i = 1 ;i<input;i++)
-                            {
-                                        String s1 = "";
-                                                if(i%3==0)
-                                                                s1 +="fizz";
-                                                        if (i%5==0)
-                                                                        s1 +="buzz";
-                                                                Thread.sleep(200);
+              for(int i = 1 ;i<input;i++)
+              {
+                   String s1 = "";
+                   if(i%3==0)
+                    s1 +="fizz";
+                   if (i%5==0)
+                     s1 +="buzz";
+              Thread.sleep(200);
                                                                         
-                                                                        if(s1=="")
-                                                                                        System.out.println(i);
-                                                                                else
-                                                                                                System.out.println(s1);
-                                                                                    }
+                  if(s1=="")
+               System.out.println(i);
+                  else
+                 System.out.println(s1);
+                                                                }
     }   
     
 }
